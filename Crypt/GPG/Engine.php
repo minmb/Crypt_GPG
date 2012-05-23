eae8e85e72ce98b87863770102d2e982ad881c72
Martin Häger <nizard@tailsp.in>
2012-05-23 04:42:24 +0300

    Relax version regexp

--- Crypt/GPG/Engine.php
+++ Crypt/GPG/Engine.php
@@ -907,7 +907,7 @@ class Crypt_GPG_Engine
             }
 
             $matches    = array();
-            $expression = '/gpg \(GnuPG\) (\S+)/';
+            $expression = '#gpg \(GnuPG[a-zA-Z0-9/]*\) (\S+)#';
 
             if (preg_match($expression, $info, $matches) === 1) {
                 $this->_version = $matches[1];
